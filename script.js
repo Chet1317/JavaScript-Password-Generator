6b871b8417d46d482cc2833e035b91701269aaf3
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-05-25 11:03:01 -0400

    added if statement to askupper case variable

--- script.js
+++ script.js
@@ -2,18 +2,25 @@
 var generateBtn = document.querySelector("#generate");
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var myNumbers = "1234567890";
+var askPasswordlength = 0;
+
 
 // Write password to the #password input
 function writePassword() {
   //var password = generatePassword();
   var passwordText = document.querySelector("#password");
   var askUppercase = confirm ("Do you want to use uppercase?");
+  if (askUppercase){
+    tempass += uppercase;
+    console.log(tempass);
+  }
   console.log("askUppercase");
   var askNumbers = confirm ("Do you want to use numbers?")
   console.log("askNumbers");
   askPasswordlength = prompt ("Choose number of characters between 8 and 128 characters");
   console.log(askPasswordlength);
   console.logn(typeof(askPasswordlength));
+  
 
 
   passwordText.value = password;
