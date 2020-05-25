32edd86ea0189fdf4de99b23ea0ade84efb1ac76
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-05-25 09:41:28 -0400

    variables for uppercase letteres and numbers

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var myNumbers = "1234567890";
 
 // Write password to the #password input
 function writePassword() {
