5d1be3ff69d2b28f3721013978d03549bc58aa9c
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-05-25 10:16:13 -0400

    added variable and consoole.log for numner of characters user would like

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ var myNumbers = "1234567890";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
   var askUppercase = confirm ("Do you want to use uppercase?");
   console.log("askUppercase");
   var askNumbers = confirm ("Do you want to use numbers?")
   console.log("askNumbers");
-  
+  askPasswordlength = prompt ("Choose number of characters between 8 and 128 characters");
+  console.log(askPasswordlength);
+  console.logn(typeof(askPasswordlength));
 
 
   passwordText.value = password;
